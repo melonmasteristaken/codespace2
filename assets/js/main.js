e0a11d0a56a38589f6aef1897a28e6bfa520ec0c
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-06-04 10:03:50 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 //firefly.js
 const fireflies = document.querySelector('.fireflies');
 
@@ -6,40 +5,27 @@ function spawnFirefly() {
   const firefly = document.createElement('div');
   firefly.classList.add('firefly');
 
-  // Random position, opacity, and scale
-  firefly.style.top = `${random(5, 20)}vh`;
-  firefly.style.left = `${random(5, 95)}vw`;
-  firefly.style.opacity = 0;
-  firefly.style.transform = `scale(${random(0.5, 1.0)})`;
-
-  // Fade-in animation
   firefly.animate([
-    { opacity: 0 },
-    { opacity: 1 }
+    { 
+      top: '100vh', 
+      left: `${random(0, 100)}vw`, 
+      opacity: 0 
+    },
+    { 
+      top: '0vh', 
+      left: `${random(0, 100)}vw + Math.sin(Math.random() * 10) * 20`, 
+      opacity: 1 
+    },
+    { 
+      top: '0vh', 
+      left: `${random(0, 100)}vw + Math.cos(Math.random() * 10) * 20`, 
+      opacity: 0 
+    }
   ], {
-    duration: 700, // 1 second
+    duration: 3000, // 3 seconds
     easing: 'ease-out'
   });
 
-  // Move and fade-out animation
-firefly.animate([
-  { 
-    top: `${random(80, 95)}vh`, 
-    left: `${random(5, 95)}vw`, 
-    opacity: 1 
-  },
-  { 
-    top: `${random(0, 100)}vh + Math.sin(Math.random() * 10) * 20`, 
-    left: `${random(0, 100)}vw + Math.cos(Math.random() * 10) * 20`, 
-    opacity: 0 
-  }
-], {
-  duration: 1700, // 3 seconds
-  easing: 'ease-out',
-  delay: 700 // Delay fade-out by 1 second
-});
-
-  // Remove firefly after animation
   firefly.addEventListener('animationend', () => {
     firefly.remove();
   });
@@ -47,8 +33,7 @@ firefly.animate([
   fireflies.appendChild(firefly);
 }
 
-// Spawn fireflies at a constant rate
-setInterval(spawnFirefly, 1000); // Spawn every 0.5 seconds
+setInterval(spawnFirefly, 500); // Spawn every 0.5 seconds
 
 function random(min, max) {
   return Math.random() * (max - min) + min;
