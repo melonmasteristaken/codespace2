bfaffd901d37405a948f78f85e075153f205e0e2
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-06-04 21:12:25 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,8 +44,27 @@ navigator.serviceWorker.getRegistrations().then(registrations => {
 
 registerServiceWorker();
 
+
 //---
 
+
+//audio.js
+const soundtrackButton = document.getElementById('soundtrack-button');
+
+soundtrackButton.addEventListener('click', () => {
+  const soundtrackId = soundtrackButton.dataset.soundtrack;
+  const soundtrackUrl = `assets/sounds/${soundtrackId}.mp3`;
+
+  const soundtrack = new Howl({
+    src: [soundtrackUrl],
+    loop: true,
+    volume: 0.5,
+  });
+  soundtrack.play();
+});
+
+
+//---
 
 
 //firefly.js
