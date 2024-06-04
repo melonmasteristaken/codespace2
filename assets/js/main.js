32929dcd963aad609894358acaa82ff4f92dc344
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-06-04 09:49:35 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,36 +7,36 @@ function spawnFirefly() {
   firefly.classList.add('firefly');
 
   // Random position, opacity, and scale
-  firefly.style.top = `${random(0, 100)}vh`;
-  firefly.style.left = `${random(0, 100)}vw`;
+  firefly.style.top = `${random(5, 95)}vh`;
+  firefly.style.left = `${random(5, 95)}vw`;
   firefly.style.opacity = 0;
-  firefly.style.transform = `scale(${random(0.5, 1.5)})`;
+  firefly.style.transform = `scale(${random(0.5, 1.0)})`;
 
   // Fade-in animation
   firefly.animate([
     { opacity: 0 },
     { opacity: 1 }
   ], {
-    duration: 1000, // 1 second
+    duration: 700, // 1 second
     easing: 'ease-out'
   });
 
   // Move and fade-out animation
   firefly.animate([
     { 
-      top: `${random(0, 100)}vh`, 
-      left: `${random(0, 100)}vw`, 
+      top: `${random(5, 95)}vh`, 
+      left: `${random(5, 95)}vw`, 
       opacity: 1 
     },
     { 
-      top: `${random(0, 100)}vh`, 
-      left: `${random(0, 100)}vw`, 
+      top: `${random(5, 95)}vh`, 
+      left: `${random(5, 95)}vw`, 
       opacity: 0 
     }
   ], {
-    duration: 3000, // 3 seconds
+    duration: 1700, // 3 seconds
     easing: 'ease-out',
-    delay: 1000 // Delay fade-out by 1 second
+    delay: 700 // Delay fade-out by 1 second
   });
 
   // Remove firefly after animation
@@ -48,7 +48,7 @@ function spawnFirefly() {
 }
 
 // Spawn fireflies at a constant rate
-setInterval(spawnFirefly, 500); // Spawn every 0.5 seconds
+setInterval(spawnFirefly, 1000); // Spawn every 0.5 seconds
 
 function random(min, max) {
   return Math.random() * (max - min) + min;
